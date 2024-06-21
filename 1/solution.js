e9d76d274d19fa307cbcab2113914efe98984091
Craig Ostrin <craigostrin@gmail.com>
2024-06-21 02:19:59 +0200

    1-solved

---
+++ 1/solution.js
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import readline from 'readline'
+
+const input = []
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt')
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    input.push(line)
+  }
+}
+
+await processLineByLine()
+
+let total = 0
+
+const test = [
+  '1abc2',
+  'pqr3stu8vwx',
+  'a1b2c3d4e5f',
+  'treb7uchet',
+]
+
+input.forEach((line) => {
+  let firstDigit
+  let secondDigit
+
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i]
+    if (!firstDigit && +char >= 0) {
+      firstDigit = char
+      secondDigit = char
+    }
+    if (+char >= 0) {
+      secondDigit = char
+    }
+  }
+  const combined = firstDigit + secondDigit
+  total += Number(combined)
+})
+
+console.log(total)
