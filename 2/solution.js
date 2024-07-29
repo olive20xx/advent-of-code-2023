a6e4270b2b7d740b0f2407bc5104f196cfec5375
Craig Ostrin <craigostrin@gmail.com>
2024-07-29 20:27:02 +0200

    solved 2 p2

--- 2/solution.js
+++ 2/solution.js
@@ -4,6 +4,9 @@ const input = await getLineByLineInput('./2/input.txt')
 
 const games = [null, ...input]
 
+// ********** PART 1 **********
+console.log('== PART 1 ==')
+
 const POSSIBLE = {
   red: 12,
   green: 13,
@@ -34,3 +37,36 @@ function countGame(game, index) {
 }
 
 console.log(total)
+
+// ********** PART 2 **********
+console.log('== PART 2 ==')
+const total2 = input
+  .map((game, index) => getPower(countMinCubes(game, index)))
+  .reduce((ac, cv) => ac + cv)
+console.log(total2)
+
+function getPower(count) {
+  return Object.values(count).reduce((t, c) => t * c)
+}
+
+function countMinCubes(game, index) {
+  if (!game) return 0
+
+  const counts = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  }
+
+  game
+    .split(': ')[1]
+    .split('; ')
+    .map((result) =>
+      result.split(', ').forEach((r) => {
+        const [number, color] = r.split(' ')
+        if (number > counts[color]) counts[color] = +number
+      })
+    )
+
+  return counts
+}
