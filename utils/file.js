44bc388c8738867fd5713669e312ffbc47291cfe
Craig Ostrin <craigostrin@gmail.com>
2024-07-29 16:47:44 +0200

    refactor: modularize input reader

---
+++ utils/file.js
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import readline from 'readline'
+
+export async function getLineByLineInput(filename) {
+  const input = []
+
+  const fileStream = fs.createReadStream(filename)
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    input.push(line)
+  }
+
+  return input
+}
